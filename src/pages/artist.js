4ed52c551f23ee2cfe28a5b586e283898d7393c6
biancacamargomachado <bianca.camargo@acad.pucrs.br>
2021-01-17 11:33:52 -0300

    feat: artist header

--- src/pages/artist.js
+++ src/pages/artist.js
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
-import IconMarker from '@material-ui/icons/Place';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 import Layout from '../components/layout';
 import Header from '../components/header';
+
+// Images import
+import image1c from '../assets/images/carousel/1.png';
 
 export default function Artist() {
   return (
@@ -10,12 +14,31 @@ export default function Artist() {
       <Header
         subHeader={
           <>
-            <div>
-              <Typography variant="h4" component="h1" color="primary">
-                Detalhes do artista
-              </Typography>
-            </div>
-            <IconMarker fontSize="large" color="primary" />
+            <Layout
+              css={{
+                display: 'flex',
+                margin: '0 auto',
+                maxWidth: 560,
+                width: '100%',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <Grid>
+                <img css={{ width: '80%', height: '80%', paddingLeft: '20%' }} src={image1c} />
+              </Grid>
+              <Grid>
+                <Typography component="h1" variant="h5" color="primary">
+                  Gabz
+                </Typography>
+              </Grid>
+              <Grid>
+                <Link href="#" onClick={() => {}} variant="body2" css={{ color: '#2F80ED' }}>
+                  Ver perfil completo
+                </Link>
+              </Grid>
+            </Layout>
           </>
         }
       />
